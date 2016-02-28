876c2a7fd13b6ca9f76b27d96b4e787252d665bf
Dominic Tarr <dominic.tarr@gmail.com>
2016-02-28 18:00:07 +1300

    tidy

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 
 module.exports = function (create) {
-  var reader = null, aborted = false, abortCb
+  var reader = null
   return function (read) {
     if('function' !== typeof read) throw new Error('read must be function')
     return function (abort, cb) {
-      if(abort) {
-        aborted = abort
-//        console.log('ABORT', reader)
-//        console.log(reader);
-        if(reader && reader.abort) {
-          console.log('abort sink')
-          reader.abort(abort, cb)
-        }
-        else {
-          console.log('ABORT READ IMMEDIATELY')
-          read(abort, cb)
-//        (reader ? reader.abort : read)(abort, cb)
-        }
-      }
-      else if(!reader)
-        (reader = create(cb))(read)
-      else {//there will only be one value, so assume this is the end
-        console.log('END NOW')
+      if(abort) (reader ? reader.abort : read)(abort, cb)
+      else if(!reader) (reader = create(cb))(read)
+      else //there will only be one value, so assume this is the end
         cb(true)
-      }
     }
   }
 }
+
+
+
+
+
+
+
 
 
 
